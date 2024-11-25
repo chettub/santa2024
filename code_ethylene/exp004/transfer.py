f5e54c91fc8aca26cb4848e21c3f548d895e7c8e
chettub <khorie4900@gmail.com>
2024-11-26 00:40:58 +0900

    initial commit

---
+++ code_ethylene/exp004/transfer.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+import pandas as pd
+
+path_input_csv = Path("../../input/santa-2024/sample_submission.csv")
+path_save = Path("./save")
+path_save.mkdir(parents=True, exist_ok=True)
+path_model = Path("../../input/gemma-2/")
+
+df = pd.read_csv(path_input_csv)
+
+for n_exp in range(1, 3):
+    n_exp = 1
+    # 後で n_exp=1 も計算
+
+    path_save_bef = Path(f"../exp{n_exp:03d}/save")
+
+    from evaluation import PerplexityCalculator
+
+    calculator = PerplexityCalculator(model_path=str(path_model))
+
+    from util import save_text
+
+    for n_idx in range(len(df)):
+        n_idx = 3
+
+        path_save_idx = path_save / f"{n_idx:04d}"
+        path_save_idx.mkdir(parents=True, exist_ok=True)
+        path_save_bef_idx = path_save_bef / f"{n_idx:04d}"
+
+        # get all txt files
+        path_txt = path_save_bef_idx.glob("*.txt")
+        list_path_txt = list(path_txt)
+
+        # save all text
+        for path in list_path_txt:
+            text = path.read_text()
+            save_text(calculator, n_idx, text, verbose=2)
+
+        # 本来であれば break を入れずにforをまわす
+        break
+
+    break
